7f993d0bcd6c8fe3fbc5960192d7ea3604541c7b
Dean Whitten <36218923+DeanWhitten@users.noreply.github.com>
2022-04-21 21:53:54 -0400

    Hangman - initial
    initialization of the project only has an array of words and some attempt variables.

--- GuessTheNumber/src/Main.java
+++ GuessTheNumber/src/Main.java
@@ -1,4 +1,4 @@
-import java.util.Objects;
+ import java.util.Objects;
 import java.util.Scanner;
 import java.util.Random;
 
---
+++ Hangman/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Random;
+
+public class Main{
+
+    public static String[] wordBank = {
+            "cat", "dog", "frog", "log", "bog", "lizard", "wizard", "blizzard",
+            "humans", "pizza", "donut", "rug", "bathtub", "rock", "pineapple",
+            "turtle", "america", "donkey", "swamp", "shrek"
+    };
+    public static Random randomGen = new Random();
+    //public static int maxAttempts = 6, userAttempts = 0;
+    
+
+
+
+
+
+    public static void main(String[] args) {
+    }
+}
